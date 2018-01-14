63a841fa5721d912a511b6e41dcced8f4e96fb5c
Hiumathy <hiumathy@Hiumathys-MacBook-Pro.local>
2018-01-14 06:47:08 -0600

    coins

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name coinTradingApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the coinTradingApp
+ */
+angular.module('coinTradingApp')
+  .controller('MainCtrl', function () {
+    this.title = 'Cryptocurrency Exchange';
+    this.currentCoin = 0;
+    this.exchangeCoin = 0;
+    this.equivPrice = -1;
+    this.limitOrder = 0;
+    this.equivOrder = 0;
+    this.calcEquivPrice = function() {
+      this.equivPrice = parseFloat(this.currentCoin)/parseFloat(this.exchangeCoin);
+    };
+    this.calcEquivOrder = function() {
+      this.equivOrder = this.equivPrice*this.limitOrder/parseFloat(this.currentCoin);
+    }
+  });
