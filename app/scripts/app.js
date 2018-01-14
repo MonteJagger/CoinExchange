db2a7af38af946c6ba3f50bc33aa68341ecf0a9c
Hiumathy <hiumathy@Hiumathys-MacBook-Pro.local>
2018-01-14 08:10:54 -0600

    coins

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name coinTradingApp
+ * @description
+ * # coinTradingApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('coinTradingApp', []);
